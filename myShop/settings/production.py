597cf3f543435f2918b8e46a841c55910b444d54
amithah <amithah.nithin@gmail.com>
2021-09-25 12:47:57 +0400

    .

--- myShop/settings/production.py
+++ myShop/settings/production.py
@@ -2,4 +2,4 @@ from myShop.settings.common import *
 
 DEBUG = False
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALLOWED_HOSTS = ['127.0.0.1', '.ec2-54-90-75-96.compute-1.amazonaws.com', '54.90.75.96', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.ec2-52-91-52-24.compute-1.amazonaws.com', '52.91.52.24', '0.0.0.0', 'localhost']
