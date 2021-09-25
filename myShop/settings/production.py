736f2ac1ea7e5eb8741ea549b772c025103cda10
amithah <amithah.nithin@gmail.com>
2021-09-25 11:09:29 +0400

    Added payment gateway

--- myShop/settings/production.py
+++ myShop/settings/production.py
@@ -1,4 +1,5 @@
 from myShop.settings.common import *
-DEBUG =False
-SECRET_KEY =os.getenv("SECRET_KEY")
-ALLOWED_HOSTS = ['127.0.0.1','.ec2-54-90-75-96.compute-1.amazonaws.com','54.90.75.96','0.0.0.0','localhost']
+
+DEBUG = False
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALLOWED_HOSTS = ['127.0.0.1', '.ec2-54-90-75-96.compute-1.amazonaws.com', '54.90.75.96', '0.0.0.0', 'localhost']
--- myShop/wsgi.py
+++ myShop/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('~/django-Eshop')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
 
 application = get_wsgi_application()
