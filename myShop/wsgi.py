7ba242b9c6e1390226f095d324ed96c3d7fae1ec
amithah <amithah.nithin@gmail.com>
2021-09-25 11:12:43 +0400

    Added payment gateway

--- myShop/wsgi.py
+++ myShop/wsgi.py
@@ -16,6 +16,6 @@ from dotenv import load_dotenv
 project_folder = os.path.expanduser('~/django-Eshop')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
 
 application = get_wsgi_application()
